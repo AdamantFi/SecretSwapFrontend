71d239b35cffd6d02afaa00d531729c3fcd2fef7
Paul Menexas <pmenexas13@gmail.com>
2022-06-14 11:41:49 -0400

    new video link switched in with shorter and more accurate info

--- src/components/BaseContainer/Card/HeroCard/HeroVideo.jsx
+++ src/components/BaseContainer/Card/HeroCard/HeroVideo.jsx
@@ -7,7 +7,7 @@ function HeroVideo() {
     <div className="background-glow animate-enter">
         <div className="video animate-enter">
             <video
-            src="https://cdn.animaapp.com/projects/622953a0a997594c4e216441/files/what-is-sefi-slides-only-720px.mp4"
+            src="https://cdn.animaapp.com/projects/622953a0a997594c4e216441/files/v3-1.mp4"
             loop
             controls
             autoPlay="autoplay"
